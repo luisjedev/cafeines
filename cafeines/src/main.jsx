43a19c73dde648defe850114bb73b99a573fdfcf
Luis Gómez <55402700+luisjedev@users.noreply.github.com>
2022-12-25 14:05:52 +0100

    Suspense

--- cafeines/src/main.jsx
+++ cafeines/src/main.jsx
@@ -5,7 +5,9 @@ import App from './App'
 import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.Suspense>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.Suspense>
 )
