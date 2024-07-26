5bd9ffbdec5834cd6f67405d9b97f76b3e18c7f7
Luis Gómez <55402700+luisjedev@users.noreply.github.com>
2024-07-26 19:40:29 +0200

    redirección página nueva

--- cafeines/src/App.jsx
+++ cafeines/src/App.jsx
@@ -1,29 +1,11 @@
-import ListaCategorias from "./components/ListaCategorias";
-import ListaProductos from "./components/ListaProductos";
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import AgregarProducto from "./pages/AgregarProducto";
-import AgregarCategoria from "./pages/AgregarCategoria";
-import Administracion from "./pages/Administracion";
-
 function App() {
-
   return (
-    <>
-        <Routes>
-          <Route path='/' element={<ListaCategorias/>}/>
-          <Route path='/:categoria' element={<ListaProductos/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='*' element={<NotFound/>} />
-          <Route path="/nuevo_producto" element={<AgregarProducto/>} />
-          <Route path="/nueva_categoria" element={<AgregarCategoria/>} />
-          <Route path="/admin" element={<Administracion/>} />
-          {/* <Route path='/:categoria/:id' element={<FichaProducto/>}/> */}
-        </Routes>
-    </>
-  )
+    <main className="w-full h-[100vh] flex justify-center items-center">
+      <button className="bg-orange-600 shadow-lg text-white p-4 rounded-md">
+        <a href="https://banquet-client.vercel.app/1">Visita nuestra carta</a>
+      </button>
+    </main>
+  );
 }
 
-export default App
-
+export default App;
--- cafeines/src/main.jsx
+++ cafeines/src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import Layout from './components/Layout'
-import './main.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Suspense>
-      <BrowserRouter>
-        <Layout Children={<App/>}>
-        </Layout>
-      </BrowserRouter>
+    <App />
   </React.Suspense>
-)
+);
